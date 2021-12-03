056629532b227c68514139037c28498c304a901e
weaverth <weaverth@bc.edu>
2021-12-03 15:38:45 -0500

    Initial Commit

---
+++ Logd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Logd
+//
+//  Created by Teddy Weaver on 12/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
